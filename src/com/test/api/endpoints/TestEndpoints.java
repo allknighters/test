763fcdf052dcf25eafbe310dcb5ce98e297170fe
swati.goyal <swati_goyal@outlook.com>
2020-09-05 12:45:26 +0530

    API test for book review app

--- src/com/test/api/endpoints/TestEndpoints.java
+++ src/com/test/api/endpoints/TestEndpoints.java
@@ -1,4 +1,17 @@
 package com.test.api.endpoints;
 
+import com.jayway.restassured.response.Response;
+import static com.jayway.restassured.RestAssured.given;
+
 public class TestEndpoints {
+
+    public static Response getBookByIsbn(String url, String isbn, int responseCode) {
+
+        String endPoint = url + "api/"+ isbn;
+
+        return given().log().ifValidationFails().header("Content-Type", "application/json")
+                .urlEncodingEnabled(false)
+                .when().get(endPoint)
+                .then().assertThat().statusCode(responseCode).extract().response();
+    }
 }
--- src/com/test/api/scripts/TestAPI.java
+++ src/com/test/api/scripts/TestAPI.java
@@ -1,7 +1,48 @@
 package com.test.api.scripts;
 
+import com.jayway.restassured.response.Response;
+import com.test.api.endpoints.TestEndpoints;
+import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+
+import java.util.HashMap;
+
+
 public class TestAPI {
+    private Logger logger = LogManager.getLogger();
+
+    @SuppressWarnings("unchecked")
+    @Test(priority = 1)
+    public void testBookByIsbnAPI() {
+        logger.info("Started executing testBookByIsbnAPI");
+
+        String url = "https://flask-bookread-app.herokuapp.com/";
+        HashMap isbns = new HashMap() {
+            {
+                put("0679889175", "First Test");
+                put("1439199353", "The Boston Girl");
+                put("0330247042", "Marathon Man");
+                put("0385743602", "Calamity");
+                put("0689862210", "The View from Saturday");
+                put("0440237556", "The Dark Highlander");
+                put("0061969559", "I Am Number Four");
+                put("184756190X", "A Girl Like You");
+                put("1561797464", "A Christmas Carol");
+                put("006440174X", "Good Night, Mr. Tom");
+            }
+        };
+
+        for (Object isbn : isbns.keySet()
+        ) {
+
+            Response response = TestEndpoints.getBookByIsbn(url, isbn.toString(), 200);
+            Assert.assertEquals(response.jsonPath().get("title").toString(), isbns.get(isbn));
+
+        }
 
 
+    }
 
 }
